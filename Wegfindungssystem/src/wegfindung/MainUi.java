9c878f6ae2310c6a548ffbf2b27a203682fb4e59
Noah <n.schafstall@hotmail.com>
2021-01-21 11:03:36 +0100

    Anfang an der UI

--- Wegfindungssystem/src/wegfindung/MainUi.java
+++ Wegfindungssystem/src/wegfindung/MainUi.java
@@ -3,10 +3,18 @@ package wegfindung;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class MainUi {
 
 	private JFrame frame;
+	private JTextField test;
 
 	/**
 	 * Launch the application.
@@ -31,13 +39,38 @@ public class MainUi {
 		initialize();
 	}
 
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(0, 0, 1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btn_login = new JButton("Login");
+		btn_login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				JFrame Login = new JFrame();
+				Login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				Login.setBounds(500, 500, 500, 500);
+				Login.setVisible(true);
+				
+				
+				
+			}
+		});
+		btn_login.setBorder(new LineBorder(new Color(0, 0, 0), 3));
+		btn_login.setFont(new Font("Century Gothic", Font.BOLD, 20));
+		btn_login.setBounds(10, 11, 98, 35);
+		frame.getContentPane().add(btn_login);
+		
+		test = new JTextField();
+		test.setBounds(121, 121, 86, 20);
+		frame.getContentPane().add(test);
+		test.setColumns(10);
 	}
-
 }
