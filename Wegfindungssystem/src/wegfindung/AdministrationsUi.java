0dcd0b9a45b20496060bb88941eda9264a4a0016
Noah <n.schafstall@hotmail.com>
2021-01-21 10:39:27 +0100

    Kleine Design Changes

--- Wegfindungssystem/src/wegfindung/AdministrationsUi.java
+++ Wegfindungssystem/src/wegfindung/AdministrationsUi.java
@@ -31,12 +31,13 @@ public class AdministrationsUi extends JFrame {
 	 * Create the frame.
 	 */
 	public AdministrationsUi() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 1920, 1080);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
