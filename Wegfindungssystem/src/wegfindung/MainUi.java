f6e08afc49016e9f0b0324a8a5be17d238e8eec4
Dominik <dominik.haag.2@gmail.com>
2021-01-28 10:55:35 +0100

    help exit button

--- Wegfindungssystem/src/wegfindung/MainUi.java
+++ Wegfindungssystem/src/wegfindung/MainUi.java
@@ -16,6 +16,8 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import java.awt.Insets;
 import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
 
 
 public class MainUi {
@@ -29,42 +31,58 @@ public class MainUi {
 		JFrame frame = new JFrame("MainUi");
 		frame.setBounds(0, 0, 1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
+		frame.getContentPane().setLayout(null);
 		
 		JButton btn_login = new JButton("Login");
+		btn_login.setBounds(10, 11, 100, 50);
 		btn_login.setMargin(new Insets(8, 14, 2, 14));
 		btn_login.setFont(new Font("Calibri", Font.BOLD, 28));
-		btn_login.setBounds(10, 11, 100, 50);
 		frame.getContentPane().add(btn_login);
 		
-		JButton btn_Hilfe = new JButton("Hilfe");
-		btn_Hilfe.setMargin(new Insets(10, 14, 2, 14));
-		btn_Hilfe.setFont(new Font("Calibri", Font.BOLD, 28));
-		btn_Hilfe.setBounds(10, 72, 100, 50);
-		frame.getContentPane().add(btn_Hilfe);
+		JButton btn_help = new JButton("Hilfe");
+		btn_help.setBounds(10, 72, 100, 50);
+		btn_help.setMargin(new Insets(10, 14, 2, 14));
+		btn_help.setFont(new Font("Calibri", Font.BOLD, 28));
+		frame.getContentPane().add(btn_help);
 		
 		JButton btn_exit = new JButton("Exit");
+		btn_exit.setBounds(10, 133, 100, 50);
 		btn_exit.setMargin(new Insets(10, 14, 2, 14));
 		btn_exit.setFont(new Font("Calibri", Font.BOLD, 28));
-		btn_exit.setBounds(10, 133, 100, 50);
 		frame.getContentPane().add(btn_exit);
 		
-		JPanel loginPanel = new JPanel();
-		loginPanel.setVisible(false);
-		loginPanel.setBorder(new LineBorder(new Color(0, 0, 0), 5));
-		loginPanel.setBounds(402, 212, 250, 150);
-		frame.getContentPane().add(loginPanel);
-		loginPanel.setLayout(null);
+		JPanel loginpanel = new JPanel();
+		loginpanel.setBounds(830, 231, 250, 150);
+		loginpanel.setVisible(false);
+		loginpanel.setBorder(new LineBorder(new Color(0, 0, 0), 5));
+		frame.getContentPane().add(loginpanel);
+		loginpanel.setLayout(null);
 		
 		JButton btn_bestaetigen = new JButton("Best\u00E4tigen");
 		btn_bestaetigen.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btn_bestaetigen.setBounds(65, 90, 120, 40);
-		loginPanel.add(btn_bestaetigen);
+		loginpanel.add(btn_bestaetigen);
+		
+		JButton btn_helpexit = new JButton("Zur\u00FCck");
 		
 		pwf_login = new JPasswordField();
 		pwf_login.setBounds(65, 43, 120, 25);
-		loginPanel.add(pwf_login);
+		loginpanel.add(pwf_login);
+		
+		JScrollPane spane_help = new JScrollPane();
+		spane_help.setBounds(339, 103, 330, 241);
+		spane_help.setBorder(new LineBorder(new Color(130, 135, 144), 5));
+		spane_help.setVisible(false);
+		frame.getContentPane().add(spane_help);
+		
+		
+		btn_helpexit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spane_help.setVisible(false);
+			}
+		});
+		spane_help.setRowHeaderView(btn_helpexit);
 		
 		
 		
@@ -77,13 +95,15 @@ public class MainUi {
 		btn_login.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
-				loginPanel.setVisible(true);
+				loginpanel.setVisible(true);
 	
 			}
 		});
 		
-		btn_Hilfe.addActionListener(new ActionListener() {
+		btn_help.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				System.out.println("Vela HAVLE VELA KUVETTE");
+				spane_help.setVisible(true);
 			}
 		});
 			
@@ -104,7 +124,7 @@ public class MainUi {
 					MitarbeiterUi MuI = new MitarbeiterUi();
 					MuI.initialize();
 				}else {
-					JOptionPane.showMessageDialog(loginPanel, "Entweder wurde der Pin falsch eingetippt \noder dieser existiert nicht.", "Falscher Pin", JOptionPane.DEFAULT_OPTION);
+					JOptionPane.showMessageDialog(loginpanel, "Entweder wurde der Pin falsch eingetippt \noder dieser existiert nicht.", "Falscher Pin", JOptionPane.DEFAULT_OPTION);
 		
 				}
 				
